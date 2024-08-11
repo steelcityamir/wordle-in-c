39e42aa79b7a8cc0ab9887d92611bd3114692c2a
Amir Boroumand <amir@wireprivacy.org>
2024-08-11 19:27:23 -0400

    Update wordle.h

--- wordle.h
+++ wordle.h
@@ -10,6 +10,13 @@
 #define CORRECT_LETTER_CORRECT_POSITION 2
 #define CORRECT_LETTER_WRONG_POSITION 1
 
+// ANSI escape codes for colors
+#define RESET_COLOR "\033[0m"
+#define GREEN_BACKGROUND "\033[42m"
+#define YELLOW_BACKGROUND "\033[43m"
+#define GREY_BACKGROUND "\033[100m"
+#define WHITE_TEXT "\033[97m"
+
 // Function declarations
 void check_guess(const char *secret, const char *guess, int *result);
 char* choose_random_word(const char* filename);
