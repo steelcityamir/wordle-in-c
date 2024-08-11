17fa218840137b873a599e6b76613f67a78f4851
Amir Boroumand <amir@wireprivacy.org>
2024-08-11 19:27:27 -0400

    Update wordle.c

--- wordle.c
+++ wordle.c
@@ -50,20 +50,6 @@
 #include <time.h>
 #include "wordle.h"
 
-#define WORD_LENGTH 5
-#define MAX_ATTEMPTS 6
-#define MAX_WORDS 1500 // Maximum number of words that can be stored
-
-#define CORRECT_LETTER_CORRECT_POSITION 2
-#define CORRECT_LETTER_WRONG_POSITION 1
-
-// ANSI escape codes for colors
-#define RESET_COLOR "\033[0m"
-#define GREEN_BACKGROUND "\033[42m"
-#define YELLOW_BACKGROUND "\033[43m"
-#define GREY_BACKGROUND "\033[100m"
-#define WHITE_TEXT "\033[97m"
-
 void check_guess(const char *secret, const char *guess, int *result) {
     bool letter_used[WORD_LENGTH] = {false}; // Track used letters in the secret word
 
